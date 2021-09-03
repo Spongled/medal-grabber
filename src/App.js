8ab3b59ec6c43b060a99c61c6272ffdb45a75b00
Jimmeh <jimdbob@hotmail.com>
2021-09-03 23:20:44 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -5,27 +5,47 @@ const Wrapper = styled.div`
   position: fixed;
   min-width: 100vw;
   min-height: 100vh;
-  background: linear-gradient(#1e1e2f, #1e1e24);
   background-image: url(https://cdn.medal.tv/games/background/background-default.png);
   background-attachment: fixed;
   background-repeat: no-repeat;
   background-size: cover;
   top: 0px;
   left: 0px;
-  box-shadow: inset 0 0 0 1000px rgb(0 0 0 / 50%) //This colour is applied over the background image. The image is transparent, meaning there's also a colour behind it in on the body tag. This is located in index.css;
+  box-shadow: inset 0 0 0 1000px rgb(0 0 0 / 50%); // This colour is applied over the background image. The image is transparent, meaning there's also a colour behind it in on the body tag. This is located in index.css
 `
 
 const Container = styled.div`
   position: relative;
-
+  border-top: 2px solid #ffb84b;
+  display: flex;
+  box-sizing: border-box;
+  max-width: 100%;
+  min-width: 0px;
+  min-height: 0px;
+  flex-direction: column;
 `
+
+const Content = styled.div`
+  display: grid;
+  box-sizing: border-box;
+  align-content: flex-start;
+  grid-template-areas:
+      ". header header ."
+      ". main aside ."
+      ". footer footer .";
+  grid-template-columns: auto 736px 400px auto;
+  grid-template-rows: auto auto auto;
+`
+
 
 function App() {
   return (
     <ErrorBoundary>
       <Wrapper>
         <Container>
-          Text
+          <Content>
+            Test
+          </Content>
         </Container>
       </Wrapper>
     </ErrorBoundary>
