54eb86b313f2770462cf003f808f0ae03b933fa0
Jimmeh <jimdbob@hotmail.com>
2021-09-16 00:07:09 +0100

    Darken background

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const MainPanel = styled.div`
   top: 0px;
   min-width: 100vw;
   min-height: 100vh;
-  box-shadow: inset 0 0 0 1000px rgb(255 255 255 / 5%);
+  box-shadow: inset 0 0 0 1000px rgb(255 255 255 /4%);
   // box-shadow: inset 0 0 0 1000px rgb(12 16 18 / 30%);
   // box-shadow: inset 0 0 0 1000px rgb(0 0 0 / 50%);
   // background-color: rgb(0 0 0 / 100%);
