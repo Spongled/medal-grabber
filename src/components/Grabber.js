c4bce4bc7275293afd497715fb7d08fa9e7f6b97
Jimmeh <jimdbob@hotmail.com>
2021-09-16 19:38:09 +0100

    Added scaling for the header

--- src/components/Grabber.js
+++ src/components/Grabber.js
@@ -11,19 +11,31 @@ import { Collapse } from 'react-collapse'
 
 const HeaderContainer = styled.div`
   display: flex;
-  margin-top: 2.5rem;
+  margin-top: 5rem;
   justify-content: center;
+
+  @media screen and (max-width: 768px){
+    margin-top: 2.5rem;
+  }
 `
 const MedalLogo = styled.img`
   vertical-align: middle;
   width: 70px;
   margin-right: 15px;
+
+  @media screen and (max-width: 768px){
+    width: 50px;
+  }
 `
 const GrabberTitle = styled.header`
   vertical-align: middle;
   font-size: 2.5rem;
   letter-spacing: 8px;
   color: #fff;
+
+  @media screen and (max-width: 768px){
+    font-size: 1.8rem;
+  }
 `
 const OptionsContainer = styled.div`
   // background: rgba(29, 31, 40, 0.75); // Brand styling.
